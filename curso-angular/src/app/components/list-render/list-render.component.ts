75d4dc4f29120d6246f0f78a8b6f3860f3fc497e
Breno Gomes <brennogomez9@gmail.com>
2022-04-01 17:19:56 -0300

    feat: services

--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListServiceService } from 'src/app/services/list-service.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -18,13 +20,18 @@ export class ListRenderComponent implements OnInit {
 
   animalDetails = '';
 
-  constructor() { }
+  constructor(private listService: ListServiceService) { }
 
   ngOnInit(): void {
   }
 
   showAge(animal: Animal) : void {
     this.animalDetails = `O ${animal.type} ${animal.name} tem ${animal.age} anos de idade!`
+  }
+
+  removeAnimal(animal: Animal) {
+    console.log("removendo animal")
+    this.animals = this.listService.remove(this.animals, animal);
   }
 
 }
